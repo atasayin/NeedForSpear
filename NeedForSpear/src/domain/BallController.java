77c6efb0e922b866025e8c9bc9a696a2a7b98eb3
denizerdogan <derdogan18@ku.edu.tr>
2021-11-08 17:01:51 +0300

    Implement partially the ball controller and the ball. Further discussion is required to decide on the format of layout.

---
+++ NeedForSpear/src/domain/BallController.java
@@ -0,0 +1,49 @@
+package domain;
+
+public class BallController {
+    public Ball ball;
+    public int gravity;
+    public int yVelocity, xVelocity;
+
+    // Two constructors for the controller, we will be using whichever is more useful.
+    public BallController() {
+        this.ball = new Ball();
+        this.gravity = 0;
+        this.xVelocity = 0;
+        this.yVelocity = 0;
+    }
+
+    public BallController(int xVelocity, int yVelocity, int gravity) {
+        this.xVelocity = xVelocity;
+        this.yVelocity = yVelocity;
+        this.gravity = gravity;
+        this.ball = new Ball();
+    }
+
+    //Game handler will delegate the controller to update the velocity
+    public void updateVelocity() {
+        this.yVelocity += this.gravity;
+    }
+
+
+    // Game handler commands the ball controller to update the position
+    // This method takes the ball position and screen dimensions from the GameState, updates it according to the velocity,
+    // and returns it.
+    public PosVector updatePosition(PosVector ballPos) {
+        ballPos.setX( ballPos.getX() + this.xVelocity );
+        ballPos.setY( ballPos.getY() + this.yVelocity );
+        return ballPos;
+    }
+
+    // When the ball reflects from a vertical surface, it reverses its velocity in x-direction.
+    // When a collision of this type is detected, the handler will command the ball controller to reverse the x-velocity.
+    public void reflectFromVertical() {
+        this.xVelocity *= -1;
+    }
+
+    // When the ball reflects from a horizontal surface, it reverses its velocity in y-direction.
+    // When a collision of this type is detected, the handler will command the ball controller to reverse the y-velocity.
+    public void reflectFromHorizontal() {
+        this.yVelocity *= -1;
+    }
+}
---
+++ NeedForSpear/src/domain/GameState.java
@@ -0,0 +1,11 @@
+package domain;
+
+public class GameState {
+    public PosVector ballPos, paddlePos;
+    public Layout layout;
+
+
+    public GameState() {
+
+    }
+}
