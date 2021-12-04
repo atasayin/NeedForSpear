1de0d67d7186fc410680dba92a45e0b71ce509f0
beyzanurcoban <bcoban17@ku.edu.tr>
2021-12-04 14:20:49 +0300

    assets added for the obstacles

--- NeedForSpear/src/UI/SimpleObstacleView.java
+++ NeedForSpear/src/UI/SimpleObstacleView.java
@@ -1,6 +1,9 @@
 package UI;
 
 import domain.obstacle.Obstacle;
+import domain.obstacle.PandorasBox;
+import domain.obstacle.SteinsGate;
+import domain.obstacle.WallMaria;
 import domain.util.PosVector;
 
 
@@ -9,12 +12,18 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 import domain.*;
 
 public class SimpleObstacleView  implements Drawable {
 
         //Obstacle obstacle;
         BufferedImage simple_obs_img ;
+        BufferedImage firm_obs_img;
+        BufferedImage explosive_obs_img;
+        BufferedImage gift_obs_img;
 
         static SimpleObstacleView instance;
 
@@ -35,81 +44,106 @@ public class SimpleObstacleView  implements Drawable {
 
         private void fillImgs() throws IOException {
             try {
-                simple_obs_img = ImageIO.read(this.getClass().getResource("../assets/paddle1.png"));
+                simple_obs_img = ImageIO.read(this.getClass().getResource("../assets/01-Breakout-Tiles.png"));
+                firm_obs_img = ImageIO.read(this.getClass().getResource("../assets/03-Breakout-Tiles.png"));
+                explosive_obs_img = ImageIO.read(this.getClass().getResource("../assets/58-Breakout-Tiles.png"));
+                gift_obs_img = ImageIO.read(this.getClass().getResource("../assets/05-Breakout-Tiles.png"));
             } catch (IOException e) {
                 e.printStackTrace();
             }
 
-            // scale to unit length L
+            // Length, thickness, and radius of the obstacles
             int obsLen = BuildModeScreen.FRAME_WIDTH/50;
-            int paddleThick = 20;
-            Image scaled = simple_obs_img.getScaledInstance(obsLen, paddleThick,
+            int obsThick = 20;
+            int circRadius = 15;
+
+            // Scale all images
+            Image scaled;
+            BufferedImage newBuffImage;
+            Graphics2D bGr;
+
+
+            // Simple obstacle scale
+            scaled = simple_obs_img.getScaledInstance(obsLen, obsThick,
                     BufferedImage.SCALE_SMOOTH);
             if (scaled instanceof BufferedImage)
                 simple_obs_img = (BufferedImage) scaled;
 
             // Create a buffered image with transparency
-            BufferedImage new_bimage = new BufferedImage(scaled.getWidth(null), scaled.getHeight(null),
+            newBuffImage = new BufferedImage(scaled.getWidth(null), scaled.getHeight(null),
                     BufferedImage.TYPE_INT_ARGB);
 
             // Draw the image on to the buffered image
-            Graphics2D bGr = new_bimage.createGraphics();
+            bGr = newBuffImage.createGraphics();
             bGr.drawImage(scaled, 0, 0, null);
             bGr.dispose();
+            simple_obs_img = newBuffImage;
 
-            simple_obs_img = new_bimage;
-//            simple_obs_img = ImageIO.read(new File("src/assets/simpleball.png"));
-//
-//            int obs_size = (int) (25 * 0.6);
-//            int offset = 10;
-//            simple_obs_img = scaleBImage(simple_obs_img, obs_size, obs_size);
+            // Firm obstacle scale
+            scaled = firm_obs_img.getScaledInstance(obsLen, obsThick,
+                    BufferedImage.SCALE_SMOOTH);
+            if (scaled instanceof BufferedImage)
+                firm_obs_img = (BufferedImage) scaled;
+
+            // Create a buffered image with transparency
+            newBuffImage= new BufferedImage(scaled.getWidth(null), scaled.getHeight(null),
+                    BufferedImage.TYPE_INT_ARGB);
+
+            // Draw the image on to the buffered image
+            bGr = newBuffImage.createGraphics();
+            bGr.drawImage(scaled, 0, 0, null);
+            bGr.dispose();
+            firm_obs_img = newBuffImage;
+
+            // Explosive obstacle scale
+            scaled = explosive_obs_img.getScaledInstance(circRadius, circRadius,
+                    BufferedImage.SCALE_SMOOTH);
+            if (scaled instanceof BufferedImage)
+                explosive_obs_img = (BufferedImage) scaled;
+
+            // Create a buffered image with transparency
+            newBuffImage= new BufferedImage(scaled.getWidth(null), scaled.getHeight(null),
+                    BufferedImage.TYPE_INT_ARGB);
+
+            // Draw the image on to the buffered image
+            bGr = newBuffImage.createGraphics();
+            bGr.drawImage(scaled, 0, 0, null);
+            bGr.dispose();
+            explosive_obs_img = newBuffImage;
+
+            // Gift obstacle scale
+            scaled = gift_obs_img.getScaledInstance(obsLen, obsThick,
+                    BufferedImage.SCALE_SMOOTH);
+            if (scaled instanceof BufferedImage)
+                gift_obs_img = (BufferedImage) scaled;
+
+            // Create a buffered image with transparency
+            newBuffImage= new BufferedImage(scaled.getWidth(null), scaled.getHeight(null),
+                    BufferedImage.TYPE_INT_ARGB);
+
+            // Draw the image on to the buffered image
+            bGr = newBuffImage.createGraphics();
+            bGr.drawImage(scaled, 0, 0, null);
+            bGr.dispose();
+            gift_obs_img = newBuffImage;
 
         }
-
-//        private BufferedImage scaleBImage(BufferedImage bimg, int width, int height) {
-//            // scale to unit length L
-//            Image scaled = bimg.getScaledInstance(width, height, BufferedImage.SCALE_SMOOTH);
-//            if (scaled instanceof BufferedImage)
-//                return (BufferedImage) scaled;
-//
-//            // Create a buffered image with transparency
-//            BufferedImage new_bimage = new BufferedImage(scaled.getWidth(null), scaled.getHeight(null),
-//                    BufferedImage.TYPE_INT_ARGB);
-//
-//            // Draw the image on to the buffered image
-//            Graphics2D bGr = new_bimage.createGraphics();
-//            bGr.drawImage(scaled, 0, 0, null);
-//            bGr.dispose();
-//
-//            return new_bimage;
-//        }
 
         @Override
         public void draw(Graphics2D g2d, DomainObject domainObject, int w, int h) {
             Obstacle obs = (Obstacle) domainObject;
 
-            g2d.drawImage(simple_obs_img, obs.getPosVector().getX(), obs.getPosVector().getY(), null);
-//            Obstacle obs = (Obstacle) object;
-//            String type = Obstacle.getType();
-//            BufferedImage bi;
-//
-//            // deciding which image to use
-//            switch (type) {
-//
-//                case "WallMaria":
-//                    bi = simple_obs_img;
-//                    break;
-//                default:
-//                    bi = simple_obs_img;
-//            }
-//
-//            // getting the location
-//            PosVector pos = obs.getPosVector();
-//
-//            // drawing the component
-//            g2d.drawImage(bi, pos.getX(), pos.getY(), null);
-//            // give width and height information of the image for collision checks
-//            obs.setWidthHeight(bi.getWidth(), bi.getHeight());
+            if (obs instanceof WallMaria){
+                g2d.drawImage(simple_obs_img, obs.getPosVector().getX(), obs.getPosVector().getY(), null);
+            } else if (obs instanceof SteinsGate){
+                g2d.drawImage(firm_obs_img, obs.getPosVector().getX(), obs.getPosVector().getY(), null);
+            } else if (obs instanceof PandorasBox){
+                g2d.drawImage(explosive_obs_img, obs.getPosVector().getX(), obs.getPosVector().getY(), null);
+            } else {
+                g2d.drawImage(gift_obs_img, obs.getPosVector().getX(), obs.getPosVector().getY(), null);
+            }
+
+
 
         }
     }
