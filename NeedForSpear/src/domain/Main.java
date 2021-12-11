e8c71fdd49bd71e6bfff4622f5a34cb22d974830
beyzanurcoban <bcoban17@ku.edu.tr>
2021-12-11 18:30:42 +0300

    main fix

--- NeedForSpear/src/domain/Main.java
+++ NeedForSpear/src/domain/Main.java
@@ -13,17 +13,11 @@ public class Main {
     static AuthorizeScreen autoMode;
 
     public static void main(String arr[]) {
-
             //main JFrame
-
             Playground nfs = new Playground();
             autoMode = new AuthorizeScreen(nfs);
             autoMode.setVisible(true);
             autoMode.addListener(nfs);
-
-            //buildMode.addListener((IRunListener) Game.getInstance().gameState.getPC());
-            //buildMode.addListener(Game.getInstance().getPlayers().get(0).getPlayerState());
-
     }
 
 }
