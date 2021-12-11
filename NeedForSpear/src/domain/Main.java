56f19f289e75b00097d34bce0b25e04071bec2c0
beyzanurcoban <bcoban17@ku.edu.tr>
2021-12-11 18:31:46 +0300

    main fix

--- NeedForSpear/src/domain/Main.java
+++ NeedForSpear/src/domain/Main.java
@@ -1,15 +1,9 @@
 package domain;
 
 import UI.AuthorizeScreen;
-import UI.BuildModeScreen;
-
 import UI.Playground;
 
-import javax.swing.*;
-
 public class Main {
-    static JFrame frame;
-    static BuildModeScreen buildMode;
     static AuthorizeScreen autoMode;
 
     public static void main(String arr[]) {
