435edc871314bed4369da6281c7687fc1bbb24a9
denizerdogan <derdogan18@ku.edu.tr>
2021-12-23 11:24:38 +0300

    Group tests 5 done.

--- NeedForSpear/src/testing/ObstacleFactoryTest.java
+++ NeedForSpear/src/testing/ObstacleFactoryTest.java
@@ -1,19 +1,17 @@
 package testing;
 
 import domain.obstacle.*;
+import org.junit.jupiter.api.BeforeEach;
 import util.PosVector;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ObstacleFactoryTest {
+    ObstacleFactory of;
 
-    @org.junit.jupiter.api.BeforeEach
+    @BeforeEach
     void setUp() {
-
-    }
-
-    @org.junit.jupiter.api.AfterEach
-    void tearDown() {
+        of = ObstacleFactory.getInstance();
     }
 
     @org.junit.jupiter.api.Test
@@ -24,28 +22,24 @@ class ObstacleFactoryTest {
     @org.junit.jupiter.api.Test
     void getWallMaria() {
         WallMaria wm = new WallMaria(400,400);
-        ObstacleFactory of = ObstacleFactory.getInstance();
         assertSame(wm.getClass(),of.getObstacle(3, new PosVector(400,400)).getClass());
     }
 
     @org.junit.jupiter.api.Test
     void getSteinsGate() {
         SteinsGate sg = new SteinsGate(400,400);
-        ObstacleFactory of = ObstacleFactory.getInstance();
         assertSame(sg.getClass(),of.getObstacle(1, new PosVector(400,400)).getClass());
     }
 
     @org.junit.jupiter.api.Test
     void getPandorasBox() {
         PandorasBox pb = new PandorasBox(400,400);
-        ObstacleFactory of = ObstacleFactory.getInstance();
         assertSame(pb.getClass(),of.getObstacle(0, new PosVector(400,400)).getClass());
     }
 
     @org.junit.jupiter.api.Test
     void getGiftofUranus() {
         GiftOfUranus gu = new GiftOfUranus(400,400);
-        ObstacleFactory of = ObstacleFactory.getInstance();
         assertSame(gu.getClass(),of.getObstacle(2, new PosVector(400,400)).getClass());
     }
 
