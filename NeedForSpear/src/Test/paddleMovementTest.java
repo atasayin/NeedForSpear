627a19baef6933c24273a50f337fa7e75b124462
ysanlik17 <ysanlik17@ku.edu.com>
2021-12-23 19:04:04 +0300

    PaddleGoLeft/Right + Rotation Tests

--- NeedForSpear/src/Test/paddleMovementTest.java
+++ NeedForSpear/src/Test/paddleMovementTest.java
@@ -2,18 +2,22 @@ package Test;
 
 import domain.DomainObject;
 import domain.Paddle;
-import domain.Ball;
+
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import util.PosVector;
 
 import java.util.ArrayList;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 public class paddleMovementTest {
 
-    Ball ball;
-    Paddle paddle;
+
+    Paddle paddle1;
+    Paddle paddle2;
     ArrayList<DomainObject> objList;
 
 
@@ -21,11 +25,13 @@ public class paddleMovementTest {
     void setUp() {
 
         System.out.println("Running: Set Up");
-        ball = new Ball();
-        paddle = new Paddle(1368, 766);
+
+        paddle1 = new Paddle(1368, 766);
+        //paddle2 = new Paddle(1368, 766);
         objList = new ArrayList<>();
-        objList.add(ball);
-        objList.add(paddle);
+
+        objList.add(paddle1);
+        //objList.add(paddle2);
 
     }
 
@@ -39,33 +45,66 @@ public class paddleMovementTest {
 
     }
 
+
     @Test
     void checkPaddleGoLeft() {
+        paddle1.setPosVector(new PosVector(360, 1100));
+        paddle1.move(1);
+        paddle1.getPosVector();
+        assertTrue(paddle1.getDx() != 360);
+
+
 
     }
 
     @Test
     void checkPaddleGoRight() {
+        paddle1.setPosVector(new PosVector(360, 1100));
+        paddle1.move(2);
+        paddle1.getPosVector();
+        assertTrue(paddle1.getDx() != 360);
 
     }
 
     @Test
     void checkPaddleRotateCClockwise() {
+        // | -45 --- 45 |
+
+       // paddle1.setPosVector(new PosVector(360, 1100));
+        paddle1.setAngle(0);
+        paddle1.move(3);
+        paddle1.getAngle();
+        assertTrue(paddle1.getAngle() != 0);
+        //assertTrue();
 
     }
 
     @Test
     void checkPaddleRotateClockwise() {
+        // | -45 --- 45 |
+        paddle1.setAngle(0);
+        paddle1.move(4);
+        paddle1.getAngle();
+        assertTrue(paddle1.getAngle() != 0);
+        //paddle1.setPosVector(new PosVector(360, 1100));
+
+        //assertTrue(paddle1.getAngle());
 
     }
 
     @Test
     void checkPaddleGoFastLeft() {
-
+        paddle1.setPosVector(new PosVector(360, 1100));
+        paddle1.move(5);
+        paddle1.getPosVector();
+        //assertTrue();
     }
 
     @Test
     void checkPaddleGoFastRight() {
+        paddle1.setPosVector(new PosVector(360, 1100));
+        paddle1.move(6);
+        paddle1.getPosVector();
 
     }
 
