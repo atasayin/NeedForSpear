69a38f0aac527c45da494b3231fff44b5b3e66b6
ysanlik17 <ysanlik17@ku.edu.com>
2021-12-23 15:38:44 +0300

    paddleMovementTest Commit1 Body

---
+++ NeedForSpear/src/Test/paddleMovementTest.java
@@ -0,0 +1,66 @@
+package Test;
+
+import domain.DomainObject;
+import domain.Paddle;
+import util.PosVector;
+import domain.Ball;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+public class paddleMovementTest {
+
+    Ball ball;
+    Paddle paddle;
+
+    DomainObject wallmaria, steinsgate, giftofuranus;
+    ArrayList<DomainObject> objList;
+
+
+    @BeforeEach
+    void setUp() {
+
+    }
+
+    @AfterEach
+    void tearDown() {
+
+
+    }
+
+    @Test
+    void checkPaddleGoLeft() {
+
+    }
+
+    @Test
+    void checkPaddleGoRight() {
+
+    }
+
+    @Test
+    void checkPaddleRotateCClockwise() {
+
+    }
+
+    @Test
+    void checkPaddleRotateClockwise() {
+
+    }
+
+    @Test
+    void checkPaddleGoFastLeft() {
+
+    }
+
+    @Test
+    void checkPaddleGoFastRight() {
+
+    }
+
+
+
+}
