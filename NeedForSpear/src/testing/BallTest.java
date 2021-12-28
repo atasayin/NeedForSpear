b3359b0c5b1bbf32f5319233b8d4b3f958722627
denizerdogan <derdogan18@ku.edu.tr>
2021-12-28 22:37:26 +0300

    tests for Ball.move() and ObstacleFactory

---
+++ NeedForSpear/src/testing/BallTest.java
@@ -0,0 +1,71 @@
+package testing;
+
+import domain.Ball;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import util.PosVector;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BallTest {
+    Ball ball;
+
+    @BeforeEach
+    void setUp() {
+        ball = new Ball();
+    }
+
+    @AfterEach
+    void tearDown() {
+        ball = null;
+    }
+
+    @Test
+    void moveWhenDead() {
+        // Test of movement of dead ball, it should not move if it is dead.
+        ball.setPosVector(new PosVector(-200, -200));
+        ball.move();
+        assertEquals(-200, ball.getPosVector().getX());
+
+    }
+
+    @Test
+    void nullPositionTest() {
+        // Test to throw exception when position is null
+        ball.setPosVector(null);
+        assertThrows(Exception.class, ()-> {
+            ball.move();});
+    }
+
+    @Test
+    void correctMovement1() {
+        int xPos = ball.getPosVector().getX();
+        int xVel = ball.xVelocity;
+        ball.setisAlive(true);
+        ball.move();
+        int lastPos = ball.getPosVector().getX();
+        assertEquals(xPos + xVel, lastPos);
+    }
+
+    @Test
+    void correctMovement2() {
+        int yPos = ball.getPosVector().getY();
+        int yVel = ball.yVelocity;
+        ball.setisAlive(true);
+        ball.move();
+        int lastPos = ball.getPosVector().getY();
+        assertEquals(yPos + yVel + 2*ball.gravity, lastPos);
+    }
+
+    @Test
+    void movementCheck() {
+        int xPos = ball.getPosVector().getX();
+        int yPos = ball.getPosVector().getY();
+        ball.setisAlive(true);
+        ball.move();
+        assertNotEquals(xPos, ball.getPosVector().getX());
+        assertNotEquals(yPos, ball.getPosVector().getY());
+
+    }
+}
